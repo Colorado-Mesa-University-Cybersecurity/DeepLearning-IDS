6fcd460dbb201ec64dacd66aa48ecadc44eb83aa
Ram Basnet <rambasnet@gmail.com>
2019-06-26 14:44:06 -0600

    added fastai-experiments.py

---
+++ fastai-expriments.py
@@ -0,0 +1,94 @@
+from fastai.tabular import *
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split, StratifiedShuffleSplit
+import os
+import sys
+import glob
+from sklearn.utils import shuffle
+
+dep_var = 'Label'
+cat_names = ['Dst Port', 'Protocol']
+cont_names = ['Timestamp', 'Flow Duration', 'Tot Fwd Pkts',
+              'Tot Bwd Pkts', 'TotLen Fwd Pkts', 'TotLen Bwd Pkts', 'Fwd Pkt Len Max',
+              'Fwd Pkt Len Min', 'Fwd Pkt Len Mean', 'Fwd Pkt Len Std',
+              'Bwd Pkt Len Max', 'Bwd Pkt Len Min', 'Bwd Pkt Len Mean',
+              'Bwd Pkt Len Std', 'Flow Byts/s', 'Flow Pkts/s', 'Flow IAT Mean',
+              'Flow IAT Std', 'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Tot',
+              'Fwd IAT Mean', 'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min',
+              'Bwd IAT Tot', 'Bwd IAT Mean', 'Bwd IAT Std', 'Bwd IAT Max',
+              'Bwd IAT Min', 'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags',
+              'Bwd URG Flags', 'Fwd Header Len', 'Bwd Header Len', 'Fwd Pkts/s',
+              'Bwd Pkts/s', 'Pkt Len Min', 'Pkt Len Max', 'Pkt Len Mean',
+              'Pkt Len Std', 'Pkt Len Var', 'FIN Flag Cnt', 'SYN Flag Cnt',
+              'RST Flag Cnt', 'PSH Flag Cnt', 'ACK Flag Cnt', 'URG Flag Cnt',
+              'CWE Flag Count', 'ECE Flag Cnt', 'Down/Up Ratio', 'Pkt Size Avg',
+              'Fwd Seg Size Avg', 'Bwd Seg Size Avg', 'Fwd Byts/b Avg',
+              'Fwd Pkts/b Avg', 'Fwd Blk Rate Avg', 'Bwd Byts/b Avg',
+              'Bwd Pkts/b Avg', 'Bwd Blk Rate Avg', 'Subflow Fwd Pkts',
+              'Subflow Fwd Byts', 'Subflow Bwd Pkts', 'Subflow Bwd Byts',
+              'Init Fwd Win Byts', 'Init Bwd Win Byts', 'Fwd Act Data Pkts',
+              'Fwd Seg Size Min', 'Active Mean', 'Active Std', 'Active Max',
+              'Active Min', 'Idle Mean', 'Idle Std', 'Idle Max', 'Idle Min']
+
+dataPath = 'CleanedTrafficData'  # use your path
+resultPath = 'results/fastai'
+
+
+def loadData(fileName):
+    dataFile = os.path.join(dataPath, fileName)
+    pickleDump = '{}.pickle'.format(dataFile)
+    if os.path.exists(pickleDump):
+        df = pd.read_pickle(pickleDump)
+    else:
+        df = pd.read_csv(dataFile)
+        df = df.dropna()
+        df = shuffle(df)
+        df.to_pickle(pickleDump)
+    return df
+
+
+def experimentIndividual(dataFile, epochs=5, normalize=False):
+    # procs = [FillMissing, Categorify, Normalize]
+    procs = [FillMissing, Categorify]
+    if normalize:
+        procs.append(Normalize)
+
+    seed = 7
+    np.random.seed(seed)
+    # load data
+    data = loadData(dataFile)
+    # define 10-fold cross validation test harness
+    kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
+    cvscores = []
+    fold = 1
+    for train_idx, test_idx in kfold.split(data.index, data[dep_var]):
+        print('running fold = ', fold)
+        fold += 1
+        # create model
+        data_fold = (TabularList.from_df(data, path=dataPath, cat_names=cat_names, cont_names=cont_names, procs=procs)
+                     .split_by_idxs(train_idx, test_idx)
+                     .label_from_df(cols=dep_var)
+                     .databunch())
+        # create model and learn
+        model = tabular_learner(
+            data_fold, layers=[200, 100], metrics=accuracy, callback_fns=ShowGraph)
+        model.fit(epochs, 1e-2)
+        model.save('{}.model'.format(os.path.basename(dataFile)))
+        # train the model, iterating on the data in batches of batch_size
+        # evaluate the model
+        loss, acc = model.validate()
+        print('loss {}: accuracy: {:.2f}%'.format(loss, acc*100))
+        cvscores.append(acc*100)
+        resultFile = os.path.join(resultPath, dataFile)
+        with open('{}.result'.format(resultFile), 'a') as fout:
+            fout.write(
+                'accuracy: {:.2f} std-dev: {:.2f}\n'.format(np.mean(cvscores), np.std(cvscores)))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: python fastai.py inputDataFile.csv')
+    else:
+        experimentIndividual(sys.argv[1])
